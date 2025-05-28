5a7a9c1799509b5c5c7b7ac705fbc2efa760058f
Steve Shilling <6329361+stevshil@users.noreply.github.com>
2025-05-28 10:08:50 +0100

    starter code for dictionaries lab 2

---
+++ Exercises/04-DataStructures/02-Dictionaries.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+
+# Put your import here
+
+todos={ "todos": [
+    {
+        "id": 1,
+        "todo": "Do something nice for someone I care about",
+        "completed": "true",
+        "userId": 26
+    },
+    {
+        "id": 2,
+        "todo": "Memorize the fifty states and their capitals",
+        "completed": "false",
+        "userId": 48
+    } ],
+    "total": 150,
+    "skip": 0,
+    "limit": 2
+}
+
+# The rest of the code goes here
