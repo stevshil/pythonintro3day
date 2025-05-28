98c25ba3c2b2f05b4418e2fd5bcac74942358be3
Steve Shilling <6329361+stevshil@users.noreply.github.com>
2025-05-28 18:19:15 +0100

    Updated MS SQL to latest pyodbc

--- Examples/12-Databases/byname.py
+++ Examples/12-Databases/byname.py
@@ -1,16 +1,21 @@
-import pymssql
+# import pymssql
+import pyodbc
 
 try:
-    conn = pymssql.connect(server='192.168.10.103', user='sa', password='P@SSw0rd123', database='steve', timeout=5)
+    # conn = pymssql.connect(server='192.168.10.103', user='sa', password='P@SSw0rd123', database='steve', timeout=5)
+    conn = pyodbc.connect(f"DRIVER={{ODBC Driver 17 for SQL Server}};SERVER=localhost\\SQLExpress;DATABASE=steve;UID=sa;PWD=secret;", timeout=5)
+    print("Connected to the database")
 except Exception as e:
     print("Unable to connect to the database")
     print("Issue is",e)
     exit(1)
 
-cursor = conn.cursor(as_dict=True)
+# cursor = conn.cursor(as_dict=True)
+cursor = conn.cursor()
 cursor.execute('SELECT * FROM users;')
 for row in cursor:
-    print("ID=%d, First Name=%s" % (row['id'], row['firstname']))
+    # print("ID=%d, First Name=%s" % (row['id'], row['firstname']))
+    print("ID=%d, First Name=%s" % (row.id, row.firstname))
 
 cursor.close()
 conn.close()
